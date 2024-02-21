d494434d0dcdbe64ff3d58aba40150da9bb4a7b9
michael <michael01gd@gmail.com>
2024-02-22 00:44:10 +0300

    yeap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am changed. Helloooo are you there???????????
+          I am changed. BTC
         </p>
         <a
           className="App-link"
